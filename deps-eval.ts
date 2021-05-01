6672f526ee70c88256ef3744618813a785725bea
Duncan Gibson <legowerewolf@gmail.com>
2021-05-01 14:56:05 -0700

    Rewrite to use hostnames for version matching

--- deps-eval.ts
+++ deps-eval.ts
@@ -1,14 +1,12 @@
 import { equal } from "https://deno.land/std@0.95.0/testing/asserts.ts";
 import { DenoInfoJson } from "./types/DenoInfoJson.ts";
 
-const regexes = {
-  DenoLand: /https?:\/\/(?<name>.*)@(?<version>[\d\.]*)/,
-};
+// for Array.filter()
+function uniques<T>(elem: T, index: number, list: Array<T>) {
+  return list.findIndex((_elem) => equal(elem, _elem)) == index;
+}
 
-const hostRegexMap = {
-  "deno.land": regexes.DenoLand,
-};
-
+// Read the dependency graph
 const graph: DenoInfoJson = await Deno.run({
   cmd: ["deno", "info", Deno.args[0], "--json", "--unstable"],
   stdout: "piped",
@@ -16,35 +14,63 @@ const graph: DenoInfoJson = await Deno.run({
   .output()
   .then((output) => JSON.parse(new TextDecoder().decode(output)));
 
+const hostnameMap: {
+  [key: string]: (
+    arg0: string,
+  ) => Promise<
+    {
+      source: string;
+      name: string;
+      savedVersion: string;
+      currentVersion: string;
+    }
+  >;
+} = {
+  "deno.land": async (spec: string) => {
+    const regex = /https?:\/\/deno.land\/(?<name>.*)@(?<version>[\d\.]*)/;
+
+    const parsed = spec.match(regex);
+
+    const name = parsed?.groups?.["name"] ?? "";
+
+    const currentVersion = await fetch(`https://deno.land/${name}`).then(
+      (resp) => resp.url.match(regex)?.groups?.["version"] ?? "",
+    );
+
+    return {
+      source: "deno.land",
+      name,
+      savedVersion: parsed?.groups?.["version"] ?? "",
+      currentVersion,
+    };
+  },
+};
+
 const data = await Promise.all(
   graph.modules
-    .map((dep) => {
-      const parsed = dep.specifier.match(regexes.DenoLand);
-
-      return {
-        name: parsed?.groups?.["name"] ?? "",
-        savedVersion: parsed?.groups?.["version"] ?? "",
-      };
-    })
-    .filter(
-      (elem, index, list) =>
-        list.findIndex((felem) => equal(elem, felem)) == index,
-    )
+    .map((dep) =>
+      hostnameMap[new URL(dep.specifier).hostname]?.(dep.specifier)
+    ),
+).then((data) =>
+  data
+    .filter((elem) => elem != null)
     .filter((elem) => elem.name != "")
-    .map(async (elem) => {
-      return {
-        ...elem,
-        currentVersion: await fetch("https://" + elem.name).then(
-          (resp) => resp.url.match(regexes.DenoLand)?.groups?.["version"],
-        ),
-      };
-    }),
+    .filter(uniques)
 );
 
 console.table(data);
 
-const outdated = data
-  .filter((elem) => [undefined, ""].indexOf(elem.currentVersion) == -1)
-  .filter((elem) => elem.currentVersion != elem.savedVersion);
+const errInfo: { [key: string]: Array<unknown> } = {
+  outdated: data
+    .filter((elem) => elem.currentVersion != "")
+    .filter((elem) => elem.currentVersion != elem.savedVersion),
+  unset: data.filter((elem) => elem.savedVersion == ""),
+  unsupportedHost: graph.modules.map((dep) => new URL(dep.specifier).hostname)
+    .filter(uniques).filter((elem) => !(elem in hostnameMap)),
+};
 
-if (outdated.length > 0) console.error(JSON.stringify(outdated));
+for (const key in errInfo) {
+  if (errInfo[key].length == 0) delete errInfo[key];
+}
+
+if (!equal(errInfo, {})) console.error(errInfo);
